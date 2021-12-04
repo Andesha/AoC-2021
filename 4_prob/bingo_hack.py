ab2b93834e8857b8aaf98290c1a742dc899b048b
Tyler Collins <tk11br@sharcnet.ca>
2021-12-04 11:20:40 -0500

    add scripts which does both

--- 4_prob/bingo_hack.py
+++ 4_prob/bingo_hack.py
@@ -5,10 +5,12 @@ with open(sys.argv[1]) as f:
 draw_order = [item for item in content.pop(0).split(',')]
 blanks = ([i for i, x in enumerate(content) if x == ''])
 
-boards = []
+parent_boards = []
 for l,r in zip(blanks, blanks[1:]):
-    boards.append([[[x,False] for x in row.split()] for row in content[l+1:r]])
+    parent_boards.append([[[x,False] for x in row.split()] for row in content[l+1:r]])
 
+has_won = [-1,-1]
+boards = parent_boards[:]
 for move in draw_order:
     for bid, board in enumerate(boards):
         for i,row in enumerate(board):
@@ -22,13 +24,21 @@ for move in draw_order:
             if (all([pair[1] for pair in row]) or
                     all([pair[1] for pair in board[mirror]])):
                 windices.append(bid)
+                if has_won[0] == -1:
+                    has_won[0] = bid
+                    has_won[1] = move
+
     if len(boards) == 1 and windices:
         break
     boards = [board for bid,board in enumerate(boards) if bid not in windices]
 
-accum = 0
+accum_lose = 0
+accum_win = 0
 for i,row in enumerate(boards[0]):
     for j,item in enumerate(row):
         if not item[1]:
-            accum += int(item[0])
-print(f'Loser score: {accum * int(move)}')
+            accum_lose += int(item[0])
+        if not parent_boards[has_won[0]][i][j][1]:
+            accum_win += int(parent_boards[has_won[0]][i][j][0])
+print(f'Winner score: {accum_win * int(has_won[1])}')
+print(f'Loser score: {accum_lose * int(move)}')
