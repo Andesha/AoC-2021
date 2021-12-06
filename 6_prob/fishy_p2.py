635b124800af91af3a7f6de6440c58515e862238
Tyler Collins <tk11br@sharcnet.ca>
2021-12-06 01:16:11 -0500

    fancy stuff

--- 6_prob/fishy_p2.py
+++ 6_prob/fishy_p2.py
@@ -5,11 +5,8 @@ with open(sys.argv[1]) as f:
 
 tank = {i:content.count(i) for i in range(9)}
 for step in range(256):
-    new_tank = {k:0 for k in range(0,9)}
-    for i in range(1,9):
-        new_tank[i-1] = tank[i]
-
-    new_tank[8] += tank[0]; new_tank[6] += tank[0]
+    new_tank = {k-1:tank[k] for k in range(1,9)}
+    new_tank[8] = tank[0]; new_tank[6] += tank[0]
     tank = new_tank
 
 print('Total fishy: ', sum(tank.values()))
