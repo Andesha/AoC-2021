d90cce4a65a4552f33fe4c1a47ad010fef729f3a
Tyler Collins <tk11br@sharcnet.ca>
2021-12-08 01:26:50 -0500

    first pass cleanup

--- 8_prob/code.py
+++ 8_prob/code.py
@@ -1,41 +1,39 @@
 import sys
 
 with open(sys.argv[1]) as f:
-    content =[[x.split(),y.split()] for x,y in [line.split(' | ') for line in f.read().splitlines()]]
+    content =[[x.split(), y.split()] for x,y in
+                [line.split(' | ') for line in f.read().splitlines()]]
 
-valid_len = [2, 3, 4, 7]
-valid_map = [1, 7, 4, 8]
+valid_len = [(1,2), (7,3), (4,4), (8,7)]
 accum = 0
 for left,right in content:
-    mapper = {k:0 for k in range(10)}
+    mapper = {}
     for word in left:
-        word_len = len(word)
-        lookup = -1
-        if word_len in valid_len:
-            lookup = valid_len.index(word_len)
-        if lookup != -1:
-            mapper[valid_map[lookup]] = word
+        for actual, seg_len in valid_len:
+            if len(word) == seg_len:
+                mapper[actual] = word
+    left = [item for item in left if item not in mapper.values()]
 
     for word in left:
-        if set(mapper[4]).issubset(set(word)) and word not in mapper.values():
-            mapper[9] = word
+        if set(mapper[4]).issubset(set(word)):
+            mapper[9] = word; left.remove(word)
     for word in left:
         word_len = len(word)
-        if word_len == 6 and set(mapper[1]).issubset(set(word)) and word not in mapper.values():
-            mapper[0] = word
+        if word_len == 6 and set(mapper[1]).issubset(set(word)):
+            mapper[0] = word; left.remove(word)
     for word in left:
         word_len = len(word)
         if word not in mapper.values() and word_len == 6:
-            mapper[6] = word
+            mapper[6] = word; left.remove(word)
     for word in left:
-        if not set(word).issubset(set(mapper[9])) and word not in mapper.values():
-            mapper[2] = word
+        if not set(word).issubset(set(mapper[9])):
+            mapper[2] = word; left.remove(word)
     for word in left:
-        if not set(word).issubset(set(mapper[6])) and word not in mapper.values():
-            mapper[3] = word
+        if not set(word).issubset(set(mapper[6])):
+            mapper[3] = word; left.remove(word)
     for word in left:
         if word not in mapper.values():
-            mapper[5] = word
+            mapper[5] = word; left.remove(word)
 
     flip_map = {''.join(sorted(v)):str(k) for k,v in mapper.items()}
     buffer = ''
